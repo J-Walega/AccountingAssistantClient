dc07d9dccc547bb74957311e34fbaf3d0056858c
JW-39615 <kubawalega1@gmail.com>
2021-03-23 18:58:54 +0100

    Migrated from branch

---
+++ AccountingAssistantClient/Requests/LoginRequest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingAssistantClient.Requests
+{
+    public class LoginRequest
+    {
+        public HttpClient Client { get; set; }
+
+        LoginRequest()
+        {
+            CreateClient();
+        }
+
+        private void CreateClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri(api);
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task Login(string login, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("login", login),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            using (HttpResponseMessage response = await Client.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<string>();
+                }
+            }
+        }
+    }
+}

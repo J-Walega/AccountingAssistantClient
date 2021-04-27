3b2094debe65b5eb7d20b9d05020ad9fe32e0a60
JW-39615 <kubawalega1@gmail.com>
2021-04-27 12:26:10 +0200

    Created expense window creation

---
+++ AccountingAssistantClient/ViewModels/ExpenseCreatorViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace AccountingAssistantClient.ViewModels
+{
+    public class ExpenseCreatorViewModel : Screen
+    {
+        private string _number;
+
+        public string Number
+        {
+            get { return _number; }
+            set 
+            { 
+                _number = value;
+                NotifyOfPropertyChange(() => Number);
+            }
+        }
+
+        private string _seller;
+
+        public string Seller
+        {
+            get { return _seller; }
+            set 
+            { 
+                _seller = value;
+                NotifyOfPropertyChange(() => Seller);
+            }
+        }
+
+        private int _nip;
+
+        public int Nip
+        {
+            get { return _nip; }
+            set 
+            { 
+                _nip = value;
+                NotifyOfPropertyChange(() => Nip);
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        private double _netto;
+
+        public double Netto
+        {
+            get { return _netto; }
+            set 
+            { 
+                _netto = value;
+                NotifyOfPropertyChange(() => Netto);
+            }
+        }
+
+        private int _vat;
+
+        public int Vat
+        {
+            get { return _vat; }
+            set 
+            { 
+                _vat = value;
+                NotifyOfPropertyChange(() => Vat);
+            }
+        }
+
+
+
+
+
+
+    }
+}

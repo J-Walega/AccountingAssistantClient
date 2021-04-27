c5bf05d4ee948f92ee941a9d6f9725c8059f856e
JW-39615 <kubawalega1@gmail.com>
2021-04-27 12:43:03 +0200

    Added example invoice

---
+++ AccountingAssistantClient/InvoiceTemplate/ExampleInvoice.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Draw;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace AccountingAssistantClient.InvoiceTemplate
+{
+    public class ExampleInvoice
+    {
+        ExampleInvoice()
+        {
+            CreateExample();
+        }
+
+        private void CreateExample()
+        {
+            // Must have write permissions to the path folder
+            PdfWriter writer = new PdfWriter("D:\\Projekty\\AccountAssistantClient\\AccountingAssistantClient\\InvoiceTemplate\\Example.pdf");
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph("HEADER")
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(20);
+
+            Paragraph subheader = new Paragraph("SUB HEADER")
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetFontSize(15);
+            document.Add(subheader);
+
+            // Line separator
+            LineSeparator ls = new LineSeparator(new SolidLine());
+            document.Add(ls);
+
+            // Table
+            Table table = new Table(2, false);
+            Cell cell11 = new Cell(1, 1)
+               .SetBackgroundColor(ColorConstants.GRAY)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("State"));
+            Cell cell12 = new Cell(1, 1)
+               .SetBackgroundColor(ColorConstants.GRAY)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("Capital"));
+
+            Cell cell21 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("New York"));
+            Cell cell22 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("Albany"));
+
+            Cell cell31 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("New Jersey"));
+            Cell cell32 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("Trenton"));
+
+            Cell cell41 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("California"));
+            Cell cell42 = new Cell(1, 1)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .Add(new Paragraph("Sacramento"));
+
+            table.AddCell(cell11);
+            table.AddCell(cell12);
+            table.AddCell(cell21);
+            table.AddCell(cell22);
+            table.AddCell(cell31);
+            table.AddCell(cell32);
+            table.AddCell(cell41);
+            table.AddCell(cell42);
+            document.Add(table);
+
+            document.Add(header);
+            document.Close();
+        }
+    }
+}
